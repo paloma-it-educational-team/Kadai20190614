33f827047394fa7e9d74852a9c2220905f5d42b1
Shingo Morimoto <shingo.morimoto@paloma.co.jp>
2019-06-20 10:58:57 +0900

    C#更新

---
+++ Csharp/LedDrive/LedDrive/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace LedDrive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(26, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ON";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "OFF";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(26, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(91, 110);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Red";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Location = new System.Drawing.Point(123, 67);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(91, 110);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Blue";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(77, 37);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "ON";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 61);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(77, 37);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "OFF";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Location = new System.Drawing.Point(220, 67);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(91, 110);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Green";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 18);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(77, 37);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "ON";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(6, 61);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(77, 37);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "OFF";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 192);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "LEDの点消灯";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Csharp/LedDrive/LedDrive/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Arduino4Net;
+using Arduino4Net.Models;
+
+namespace LedDrive
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.AddRange
+                (System.IO.Ports.SerialPort.GetPortNames());
+        }
+
+        enum TypeOfLed
+        {
+            Red = 5,
+            Blue = 6,
+            Green = 7
+        }
+        Arduino arduino;
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            arduino = new Arduino(((ComboBox)sender).SelectedItem.ToString());
+            ((ComboBox)sender).Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            arduino.DigitalWrite((int)TypeOfLed.Red,DigitalPin.High);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            arduino.DigitalWrite((int)TypeOfLed.Red, DigitalPin.Low);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            arduino.DigitalWrite((int)TypeOfLed.Blue, DigitalPin.High);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            arduino.DigitalWrite((int)TypeOfLed.Blue, DigitalPin.Low);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            arduino.DigitalWrite((int)TypeOfLed.Green, DigitalPin.High);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            arduino.DigitalWrite((int)TypeOfLed.Green, DigitalPin.Low);
+        }
+    }
+}
